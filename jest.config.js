515854b52988ff1520ecf1e4e00c1b5cfeb979fe
ADMSK\AVROGAL1 <avrogal1@mts.ru>
2021-02-04 17:45:47 +0300

    docs: updates on workflows
    Added information on documentation

--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,12 @@
 module.exports = {
   verbose: true,
   clearMocks: true,
-  moduleFileExtensions: [ 'js', 'ts' ],
+  moduleFileExtensions: [ 'ts', 'tsx', 'js', 'jsx', 'json', 'node' ],
   testEnvironment: 'node',
   testMatch: [ '**/*.test.ts' ],
   testRunner: 'jest-circus/runner',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.[tj]sx?$',
   transform: {
     "^.+\\.(ts)$": "ts-jest"
-  },
+  }
 }
